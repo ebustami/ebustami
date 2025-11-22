5f8e878d8f02cfa5454be63b9bc12487f5be0822
Eyad Bustami <102246457+ebustami@users.noreply.github.com>
2025-11-21 21:06:11 -0800

    bk to original

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   type        = string
-  default     = "us-east-1a"
+  default     = "us-east-2"
   description = "The ARN of the S3 bucket to use for the application."
 }
 variable "aws_s3_bucket_arn" {
@@ -27,4 +27,5 @@ variable "app_name" {
   type        = string
   description = "The name of the application."
 }
+
 
